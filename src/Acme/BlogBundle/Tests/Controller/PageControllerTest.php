e09a9913b5b07fd960e7c802841fef53aa4326e0
Antonio Peric <antonio@locastic.com>
2014-09-03 07:17:21 +0200

    Part2 completed

--- src/Acme/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Acme/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -17,6 +17,36 @@ class PageControllerTest extends WebTestCase
         $this->assertJsonResponse($response, 200);
     }
 
+    public function testJsonPostPageAction()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v1/pages.json',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"title":"title1","body":"body1"}'
+        );
+
+        $this->assertJsonResponse($client->getResponse(), 201, false);
+    }
+
+    public function testJsonPostPageActionShouldReturn400WithBadParameters()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v1/pages.json',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"titles":"title1","bodys":"body1"}'
+        );
+
+        $this->assertJsonResponse($client->getResponse(), 400, false);
+    }
+
     protected function assertJsonResponse($response, $statusCode = 200)
     {
         $this->assertEquals(
